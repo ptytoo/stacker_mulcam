60fc4e44c567ba29d58c9cb2373d1707d7d184e8
ptytoo <ptytoo@naver.com>
2017-12-07 10:30:47 +0900

    mypage show my stack complete

--- app/controllers/indi/users_controller.rb
+++ app/controllers/indi/users_controller.rb
@@ -1,8 +1,7 @@
 class Indi::UsersController < Indi::ApplicationController
   def index
     @user = current_user
-    @my_stack = InterStack.where(user_id: @user.id)
-    @stack = []
+    @stacks = InterStack.where(user_id: @user.id)
   end
 
 end
