2a35f232c95dcc60d10e8561c6e01eddba0b6259
GENIEYOO <forstudym.m@gmail.com>
2017-12-07 14:40:50 +0000

    jiin crawling added

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
 source 'https://rubygems.org'
-
-
 gem 'poltergeist'
 gem 'phantomjs', :require => 'phantomjs/poltergeist'
 gem 'capybara'
--- jiin_crawler.rb
+++ jiin_crawler.rb
@@ -9,14 +9,32 @@ Capybara.run_server = false
 module GetPrice
   class WebScraper
     include Capybara::DSL
-
     def get_page_data(url)
       visit(url)
       doc = Nokogiri::HTML(page.html)
-      doc.css('td.col2 span')
+      for i in 1..4 do
+      contents = doc.css('body > div.container > div > div > div > div:nth-child(2) > div:nth-child('+ "#{i}"+') > div.panel-footer > div')
+          contents.each do |content|
+            arr = content['title']
+            unless arr=='0'
+              puts arr
+            end
+          end
+      end
+      for i in 1..5 do
+      contents = doc.css('body > div.container > div > div > div > div:nth-child(3) > div:nth-child('+ "#{i}"+') > div.panel-footer > div')
+          contents.each do |content|
+            arr = content['title']
+            unless arr=='0'
+              puts arr
+            end
+          end
+      end
     end
   end
 end
 
+
+
 scraper = GetPrice::WebScraper.new
-puts scraper.get_page_data('https://web.apps.markit.com/WMXAXLP?YYY2220_zJkhPN/sWPxwhzYw8K4DcqW07HfIQykbYMaXf8fTzWT6WKnuivTcM0W584u1QRwj').map(&:text).inspect
+scraper.get_page_data('http://techstacks.io/').to_s.inspect
