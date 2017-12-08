5085bda67261d9f2fb4d667f5ed9ee5662d1f0c6
ptytoo <ptytoo@naver.com>
2017-12-08 14:16:17 +0900

    add service update show page to enter mypage

--- app/controllers/enter/companies_controller.rb
+++ app/controllers/enter/companies_controller.rb
@@ -3,6 +3,7 @@ class Enter::CompaniesController < Enter::ApplicationController
     # @services = Service.all
     @user = current_user
     @company = Company.find_by(id: @user.company_id)
+    @services = Service.where(company_id: @company.id)
   end
 
 end
