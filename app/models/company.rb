eceb94b40046a1930f59d12da5810648929a57ce
rickkim90 <rickkim90@gmail.com>
2017-12-09 18:01:18 +0900

    modify company_fields_controller

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,5 @@
 class Company < ActiveRecord::Base
   belongs_to :company_field
   has_many :services
-  mount_uploader :logo_url, CompanyLogoUploader
+  #mount_uploader :logo_url, CompanyLogoUploader
 end
