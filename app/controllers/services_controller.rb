f408325d1c85d25ea58a0f66bd19646d7b47321f
ptytoo <ptytoo@naver.com>
2017-12-09 19:35:55 +0900

    fix service show page

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -10,8 +10,6 @@ class ServicesController < ApplicationController
   # GET /services/1
   # GET /services/1.json
   def show
-    @service = Service.all
-    @services = Service.where(company_id: params[:id])
   end
 
   # GET /services/new
