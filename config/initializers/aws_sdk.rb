d1a8cd7d05f55699f1185fcecdebed4820222aff
rickkim90 <rickkim90@gmail.com>
2017-12-11 13:22:21 +0900

    SES mailer

--- config/initializers/aws_sdk.rb
+++ config/initializers/aws_sdk.rb
@@ -1,2 +1,3 @@
 creds = Aws::Credentials.new(ENV['AWS_SES_ID'], ENV['AWS_SES_SECRET'])
+puts ENV['AWS_SES_ID']
 Aws::Rails.add_action_mailer_delivery_method(:aws_sdk, credentials: creds, region: 'us-west-2')
