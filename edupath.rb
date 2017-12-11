40e6885e53a38a215924f19b00d97ed299db3745
iamdaisy <dahyee89@gmail.com>
2017-12-11 14:44:10 +0900

    to update

--- edupath.rb
+++ edupath.rb
@@ -7,7 +7,7 @@ require 'httparty'
 require 'csv'
 require 'pry'
 
-$stack_name = ["Languages", "Database"]
+# $stack_name = ["Languages", "Database"]
 
 File.open("edu_path.csv", "w") do |file|
   file.write("")
