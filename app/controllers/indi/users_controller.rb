10dbd972c7d66bbbbd30ef21300e6064a7e072de
ptytoo <ptytoo@naver.com>
2017-12-12 16:35:27 +0900

    park soo sang god

--- app/controllers/indi/users_controller.rb
+++ app/controllers/indi/users_controller.rb
@@ -6,7 +6,12 @@ class Indi::UsersController < Indi::ApplicationController
   end
 
   def regist_stack
-    stack_list = params[:ret]
-    puts stack_list
+    stack_list = params[:return_val]
+    stack_list.each do |st|
+        MyStack.create(
+          user_id: current_user.id,
+          stack_id: st
+        )
+    end
   end
 end
