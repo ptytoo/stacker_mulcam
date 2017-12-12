239801c9e2867e4bcd2cbbd343ec497e5d928819
ptytoo <ptytoo@naver.com>
2017-12-12 10:59:26 +0900

    sql string to rails style

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -20,6 +20,10 @@ class StacksController < ApplicationController
   def show
     # 해당 스택을 사용하는 서비스 리스트 보여주기
     # @result_companies = Company.where("name Like ?", "%#{params[:search]}%")
+    if current_user
+      @like_my = MyStack.where(user_id: current_user.id).where(stack_id: @stack.id).exists?
+      @like_inter = InterStack.where(user_id: current_user.id).where(stack_id: @stack.id).exists?
+    end
     @services = ServiceStack.where(stack_id: @stack.id)
   end
 
