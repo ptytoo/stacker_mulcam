5aebbead73a4c38f9083dff7c01011daa402b05e
iamdaisy <dahyee89@gmail.com>
2017-12-13 00:49:21 +0900

    block indi_user stack edit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -15,11 +15,13 @@ class Ability
       #해당 기업회원의 회사와 서비스만 추가/수정/삭제가능해아함
       can [:edit,:update], Company, id: user.company_id
       can [:create, :edit,:update], Service, id: user.company_id
+      cannot [:edit], Stack
     elsif user.indi?
       can :read, :all
       can [:index, :show, :detail], Company
       can [:index, :show], CompanyField
       can [:new, :edit, :add_stack, :register_interesting, :register_my_stack], Stack
+      cannot [:edit], Stack
       cannot [:edit, :new, :destroy], Service
     end
     #
