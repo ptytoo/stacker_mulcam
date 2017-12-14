3ad05566efea4ba764b1854d0331385edc308b46
rickkim90 <rickkim90@gmail.com>
2017-12-14 13:32:23 +0900

    merge

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :stacks
   validates_uniqueness_of :email
   validates_uniqueness_of :nickname
-
+  has_many :services
   validates :nickname, presence: {message: "닉네임을 입력해주세요!"}, length: {maximum: 15, message: "  닉네임이 너무 깁니다!"}, uniqueness: true
 
   validates :email,
