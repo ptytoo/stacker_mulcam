64861c96cdfe539e57b18a1fb8280b55aaf0ec18
GENIEYOO <forstudym.m@gmail.com>
2017-12-14 05:45:12 +0000

    commit reverting

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -11,10 +11,10 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  DELETE /resource/sign_out
+  def destroy
+    super
+  end
 
   # protected
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+           :recoverable, :rememberable, :trackable, :validatable
+           # :omniauthable
 
-  has_and_belongs_to_many :stacks
-  validates_uniqueness_of :email
-  validates_uniqueness_of :nickname
-  has_many :services
-  validates :nickname, presence: {message: "닉네임을 입력해주세요!"}, length: {maximum: 15, message: "  닉네임이 너무 깁니다!"}, uniqueness: true
+    has_and_belongs_to_many :stacks
+    validates_uniqueness_of :email
+    validates_uniqueness_of :nickname
 
-  validates :email,
-    presence: true,
-    format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+    validates :nickname, presence: {message: "닉네임을 입력해주세요!"}, length: {maximum: 15, message: "  닉네임이 너무 깁니다!"}, uniqueness: true
 
-  validates :phone,
-    presence: true,
-    format: {with: /\^[0-9]\*\$/}
+    validates :email,
+      presence: true,
+      format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+
+
+    validates :phone,
+      presence: true,
+      format: {with: /[0-9]/}
 
 
 
