35e621818bd7ba4355c44118bb56633292d9c994
ptytoo <ptytoo@naver.com>
2017-12-29 17:40:56 +0900

    change enviroment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-require 'omniauth-facebook'
 
 # Initialize the Rails application.
 Rails.application.initialize! :strategy_class
