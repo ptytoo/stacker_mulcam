3c0d1f1e6a67046121fd80a68209fb2d21778cc1
rickkim90 <rickkim90@gmail.com>
2018-01-02 16:06:17 +0900

    merge

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,7 +2,7 @@ class Service < ActiveRecord::Base
   belongs_to :company
 
   #has_and_belongs_to_many :stacks
-  mount_uploader :logo_url, ServiceLogoUploader
+  #mount_uploader :logo_url, ServiceLogoUploader
 
   has_many :impressions, :as=> :impressionable
 
