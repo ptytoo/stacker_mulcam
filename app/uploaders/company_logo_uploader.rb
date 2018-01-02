3eb67ebe5f0af85d0e16f469e70f41618e8f16da
ptytoo <ptytoo@naver.com>
2018-01-02 15:36:27 +0900

    change company logo upload

--- app/uploaders/company_logo_uploader.rb
+++ app/uploaders/company_logo_uploader.rb
@@ -11,7 +11,8 @@ class CompanyLogoUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "Company/#{model.company_field.name}/#{model.name}"
+    f_name = model.company_field.name.gsub!("/", ":")
+    "Company/#{f_name}/#{model.name}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
