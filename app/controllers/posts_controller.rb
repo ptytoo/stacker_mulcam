2d75838cb3324317007f27c103c079933f7b3f98
ptytoo <ptytoo@naver.com>
2018-02-09 18:39:08 +0900

    add post, add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    # @posts = Post.all
+    @posts = Post.page(params[:page])
   end
 
   # GET /posts/1
